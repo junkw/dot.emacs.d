d70607faca47f14487dda9e6e624871163e41110
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2012-07-29 15:08:39 +0900

    Fix el-get recipe

--- init.el
+++ init.el
@@ -79,7 +79,16 @@
 (setq el-get-verbose t)
 
 (setq el-get-sources
-      '((:name el-get :branch "master")))
+      '((:name el-get
+               :website "https://github.com/dimitri/el-get#readme"
+               :description "Manage the external elisp bits and pieces you depend upon."
+               :type github
+               :branch "master"
+               :pkgname "dimitri/el-get"
+               :features el-get
+               :info    "el-get.info"
+               :load    "el-get.el")
+        ))
 
 (if (require 'el-get nil t)
     (el-get 'sync)
