dc0b53d0b36c0713c69d31f6f8fb5067f1c93ecf
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2013-01-08 14:42:20 +0900

    Add Ediff config

---
+++ post-init-diff.el
@@ -0,0 +1,48 @@
+;;; post-init-diff.el --- Emacs init file
+
+;; Copyright (C) 2013  Jumpei KAWAMI
+
+;; Author: Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
+;; Created: Jan. 8, 2013
+;; Keywords: .emacs, diff, ediff
+
+;;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Change Log:
+;;
+
+;;; Code:
+
+(setq diff-switches "-u")               ; unified diff format
+
+(eval-after-load-q "ediff"
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain) ; don't make 'Ediff Control Panel' frame
+  (setq ediff-split-window-function 'split-window-horizontally) ; split ediff window, side by side
+  (setq ediff-keep-variants nil))       ; remove unmodified buffers
+
+;; Local Variables:
+;; mode: emacs-lisp
+;; coding: utf-8-emacs-unix
+;; indent-tabs-mode: nil
+;; byte-compile-warnings: (not free-vars unresolved cl-functions mapcar constants)
+;; End:
+
+;;; post-init-diff.el ends here
