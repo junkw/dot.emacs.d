7352bb5d796ee8cbdff1e33938dfb5344937fc6f
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2013-01-10 14:56:40 +0900

    Add migemo config

--- opt-init-packages.el
+++ opt-init-packages.el
@@ -53,8 +53,8 @@
 (defvar jkw:el-get-package-list-from-recipe
   '(ace-jump-mode auto-async-byte-compile cssm-mode ctags dired+ eldoc-extension expand-region
                   flymake-csslint flymake-html-validator flymake-phpcs geben goto-chg
-                  html5 info+ lispxmp mmm-mode paredit php-align psvn rainbow-mode recentf-ext
-                  scratch-ext sequential-command undo-tree viewer)
+                  html5 info+ lispxmp migemo mmm-mode paredit php-align psvn rainbow-mode
+                  recentf-ext scratch-ext sequential-command undo-tree viewer)
   "List of packages I use straight from recipe files")
 
 ;; Init after loading el-get
