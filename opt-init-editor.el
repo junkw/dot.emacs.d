212bb74ba9e0ae5cd788b554c8abbf1311a52329
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2013-07-26 14:52:22 +0900

    Add commands to increment and decrement an integer at point

--- opt-init-editor.el
+++ opt-init-editor.el
@@ -31,6 +31,8 @@
 
 ;;; Code:
 
+(require 'thingatpt)
+
 ;; Trun at EOL
 (setq truncate-lines nil)
 (setq truncate-partial-width-windows nil)
@@ -111,6 +113,69 @@ With argument ARG, do kill commands that many times."
                  (t
                   (beginning-of-thing 'word) (kill-word arg)))))))
 
+;; Increment and decrement an integer at point
+;; http://emacsredux.com/blog/2013/07/25/increment-and-decrement-integer-at-point/
+(defun thing-at-point-goto-end-of-integer ()
+  "Go to end of integer at point."
+  (let ((inhibit-changing-match-data t))
+    ;; Skip over optional sign
+    (when (looking-at "[+-]")
+      (forward-char 1))
+    ;; Skip over digits
+    (skip-chars-forward "[[:digit:]]")
+    ;; Check for at least one digit
+    (unless (looking-back "[[:digit:]]")
+      (error "No integer here"))))
+(put 'integer 'beginning-op 'thing-at-point-goto-end-of-integer)
+
+(defun thing-at-point-goto-beginning-of-integer ()
+  "Go to end of integer at point."
+  (let ((inhibit-changing-match-data t))
+    ;; Skip backward over digits
+    (skip-chars-backward "[[:digit:]]")
+    ;; Check for digits and optional sign
+    (unless (looking-at "[+-]?[[:digit:]]")
+      (error "No integer here"))
+    ;; Skip backward over optional sign
+    (when (looking-back "[+-]")
+      (backward-char 1))))
+(put 'integer 'beginning-op 'thing-at-point-goto-beginning-of-integer)
+
+(defun thing-at-point-bounds-of-integer-at-point ()
+  "Get boundaries of integer at point."
+  (save-excursion
+    (let (beg end)
+      (thing-at-point-goto-beginning-of-integer)
+      (setq beg (point))
+      (thing-at-point-goto-end-of-integer)
+      (setq end (point))
+      (cons beg end))))
+(put 'integer 'bounds-of-thing-at-point 'thing-at-point-bounds-of-integer-at-point)
+
+(defun thing-at-point-integer-at-point ()
+  "Get integer at point."
+  (let ((bounds (bounds-of-thing-at-point 'integer)))
+    (string-to-number (buffer-substring (car bounds) (cdr bounds)))))
+(put 'integer 'thing-at-point 'thing-at-point-integer-at-point)
+
+(defun increment-integer-at-point (&optional inc)
+  "Increment integer at point by one.
+
+With numeric prefix arg INC, increment the integer by INC amount."
+  (interactive "p")
+  (let ((inc (or inc 1))
+        (n (thing-at-point 'integer))
+        (bounds (bounds-of-thing-at-point 'integer)))
+    (delete-region (car bounds) (cdr bounds))
+    (insert (int-to-string (+ n inc)))))
+
+(defun decrement-integer-at-point (&optional dec)
+  "Decrement integer at point by one.
+
+With numeric prefix arg DEC, decrement the integer by DEC amount."
+  (interactive "p")
+  (increment-integer-at-point (- (or dec 1))))
+
 ;;; Expand
 (setq hippie-expand-try-functions-list
       '(try-expand-dabbrev
@@ -148,6 +213,8 @@ With argument ARG, do kill commands that many times."
 (global-set-key (kbd "M-/")   'hippie-expand) ; replace `dabbrev-expand'
 (global-set-key (kbd "C-S-k") 'kill-whole-line)
 (global-set-key (kbd "M-d")   'kill-word-dwim)
+(global-set-key (kbd "C-c +") 'increment-integer-at-point)
+(global-set-key (kbd "C-c -") 'decrement-integer-at-point)
 
 ;; Local Variables:
 ;; mode: emacs-lisp
