ef707c9516d02c1fc15e5302aa29263bf3376419
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2013-09-08 21:37:50 +0900

    Setup foreign-regexp

---
+++ init.d/init-foreign-regexp.el
@@ -0,0 +1,44 @@
+;;; init-foreign-regexp.el --- el-get init file for packforeign-regexpe foreign-regexp
+
+;; Copyright (C) 2013  Jumpei KAWAMI
+
+;; Author: Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
+;; Created: Sep. 8, 2013
+;; Keywords: .emacs, regex
+
+;;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Change Log:
+;;
+
+;;; Code:
+
+(setq foreign-regexp/regexp-type 'perl)
+(setq reb-re-syntax 'foreign-regexp)
+
+;; Local Variables:
+;; mode: emacs-lisp
+;; coding: utf-8-emacs-unix
+;; indent-tabs-mode: nil
+;; byte-compile-warnings: (not free-vars unresolved mapcar constants)
+;; End:
+
+;;; init-foreign-regexp.el ends here
--- opt-init-packages.el
+++ opt-init-packages.el
@@ -47,10 +47,10 @@
 
 (defvar jkw:el-get-package-list-from-recipe
   '(ace-jump-mode ag auto-async-byte-compile color-moccur cssm-mode ctags dired+ eldoc-extension
-                  expand-region flymake-csslint flymake-html-validator flymake-phpcs geben
-                  gist goto-chg grep-a-lot html5 info+ js2-mode lispxmp magit migemo mmm-mode
-                  moccur-edit org-mode paredit php-align psvn rainbow-mode recentf-ext
-                  scratch-ext sequential-command twittering-mode undo-tree viewer wgrep)
+                  expand-region flymake-csslint flymake-html-validator flymake-phpcs foreign-regexp
+                  geben gist goto-chg grep-a-lot html5 info+ js2-mode lispxmp magit migemo mmm-mode
+                  moccur-edit org-mode paredit php-align psvn rainbow-mode recentf-ext scratch-ext
+                  sequential-command twittering-mode undo-tree viewer wgrep)
   "List of packages I use straight from recipe files.")
 
 ;; Init after loading el-get
