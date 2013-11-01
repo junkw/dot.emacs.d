b1117645cb47e13679883987d7ad99899dbf73e9
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2013-11-01 12:58:40 +0900

    Setup yasnippet and helm-c-yasnippet
    Fix yasnippet recipe with el-get-sources

---
+++ init.d/init-yasnippet.el
@@ -0,0 +1,46 @@
+;;; init-yasnippet.el --- el-get init file for package yasnippet
+
+;; Copyright (C) 2013  Jumpei KAWAMI
+
+;; Author: Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
+;; Created: Oct. 28, 2013
+;; Keywords: .emacs, snippet
+
+;;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Change Log:
+;;
+
+;;; Code:
+
+(require 'pre-init-core)
+
+(add-to-list 'yas-snippet-dirs "~/.emacs.d/etc/yasnippet")
+(add-hooks '(prog-mode org-mode mu4e-compose-mode) 'yas-minor-mode)
+
+;; Local Variables:
+;; mode: emacs-lisp
+;; coding: utf-8-emacs-unix
+;; indent-tabs-mode: nil
+;; byte-compile-warnings: (not free-vars unresolved mapcar constants)
+;; End:
+
+;;; init-yasnippet.el ends here
--- opt-init-packages.el
+++ opt-init-packages.el
@@ -53,16 +53,17 @@
         (:name popwin :features popwin)
         (:name powerline :autoloads nil)
         (:name undo-tree :features undo-tree)
+        (:name yasnippet :compile "yasnippet.el")
         ))
 
 (defvar jkw:el-get-package-list-from-recipe
   '(ace-jump-mode ag all-ext anzu auto-async-byte-compile cl-lib-highlight color-moccur
                   cssm-mode ctags dired+ eldoc-extension expand-region flymake-csslint
                   flymake-html-validator flymake-phpcs foreign-regexp geben gist
-                  git-gutter-fringe goto-chg grep-a-lot html5 helm-descbinds helm-migemo
-                  highlight-symbol info+ js2-mode linum-relative lispxmp magit migemo
-                  mmm-mode moccur-edit monokai-emacs org-mode php-align php-mode psvn
-                  rainbow-mode recentf-ext scratch-ext sequential-command tern
+                  git-gutter-fringe goto-chg grep-a-lot html5 helm-c-yasnippet
+                  helm-descbinds helm-migemo highlight-symbol info+ js2-mode linum-relative
+                  lispxmp magit migemo mmm-mode moccur-edit monokai-emacs org-mode php-align
+                  php-mode psvn rainbow-mode recentf-ext scratch-ext sequential-command tern
                   twittering-mode viewer wgrep)
   "List of packages I use straight from recipe files.")
 
