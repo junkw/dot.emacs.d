0f4e152d639ca53595c0888d1fb4ad496311955f
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2013-12-13 01:20:10 +0900

    Add command `dired-do-convert-coding-system'

--- opt-init-file.el
+++ opt-init-file.el
@@ -48,12 +48,12 @@
 ;; Dired mode
 (require 'wdired)
 (setq wdired-allow-to-change-permissions t)
-(define-key dired-mode-map (kbd "R") 'wdired-change-to-wdired-mode)
 
 (require 'ls-lisp)
 (setq ls-lisp-use-insert-directory-program nil)
 (setq ls-lisp-dirs-first t)
 
+(require 'dired-aux)
 ;; Compression and uncompression with atool
 ;; http://d.hatena.ne.jp/mooz/20110911/p1
 (defvar jkw:dired-additional-compression-suffixes
@@ -62,11 +62,56 @@
     ".tar" ".tbz" ".tbz2" ".tgz" ".tlz" ".txz" ".tzo" ".war" ".xz" ".zip")
   "Suffixes for compression and uncompression adding in `dired-compress-file-suffixes'.")
 
-(eval-after-load* 'dired-aux
-  (when (executable-find "atool")
-    (cl-loop for suffix in jkw:dired-additional-compression-suffixes
-             do (add-to-list 'dired-compress-file-suffixes
-                             `(,(concat "\\" suffix "\\'") "" "aunpack")))))
+(when (executable-find "atool")
+  (cl-loop for suffix in jkw:dired-additional-compression-suffixes
+           do (add-to-list 'dired-compress-file-suffixes
+                           `(,(concat "\\" suffix "\\'") "" "aunpack"))))
+
+;; Batch convert coding system
+;; http://www.bookshelf.jp/soft/meadow_25.html#SEC273
+(defvar dired-default-file-coding-system nil
+  "Default coding system for converting file (s).")
+
+(defvar dired-file-coding-system 'no-conversion)
+
+(defun dired-convert-coding-system ()
+  "Function for converting coding system on dired."
+  (let ((file (dired-get-filename))
+        (coding-system-for-write dired-file-coding-system)
+        failure)
+    (condition-case err
+        (with-temp-buffer
+          (insert-file-contents file)
+          (write-region (point-min) (point-max) file))
+      (error (setq failure err)))
+    (if (not failure)
+        nil
+      (dired-log "convert coding system error for %s:\n%s\n" file failure)
+      (dired-make-relative file))))
+
+(defun dired-do-convert-coding-system (coding-system &optional arg)
+  "Convert file (s) in specified CODING-SYSTEM.
+
+ARG is like in `dired-map-over-marks'."
+  (interactive
+   (list (let ((default (or dired-default-file-coding-system
+                            buffer-file-coding-system)))
+           (read-coding-system
+            (format "Coding system for converting file (s) (default, %s): " default)
+            default))
+         current-prefix-arg))
+  (check-coding-system coding-system)
+  (setq dired-file-coding-system coding-system)
+  (dired-map-over-marks-check
+   (function dired-convert-coding-system) arg 'convert-coding-system t))
+
+;; Keymap
+(defvar jkw:dired-mode-map
+  '(("R" . wdired-change-to-wdired-mode)
+    ("E" . dired-do-convert-coding-system))
+  "My keybinds for dired mode.")
+
+(define-keys dired-mode-map jkw:dired-mode-map)
 
 ;; Local Variables:
 ;; mode: emacs-lisp
