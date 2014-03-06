f70ced5caa6591271ac71e4917c01545aa5caf9f
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2014-03-06 17:01:48 +0900

    Add helm-swoop recipe and install it

---
+++ init.d/init-helm-swoop.el
@@ -0,0 +1,48 @@
+;;; init-helm-swoop.el --- el-get init file for package helm-swoop
+
+;; Copyright (C) 2014  Jumpei KAWAMI
+
+;; Author: Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
+;; Created: Mar. 12, 2014
+;; Keywords: .emacs
+
+;;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Change Log:
+;;
+
+;;; Code:
+
+(setq helm-swoop-speed-or-color t)
+
+;;;; Keymap
+(global-set-key (kbd "C-x c M-i") 'helm-swoop)
+(global-set-key (kbd "C-x c M-I") 'helm-multi-swoop)
+(global-set-key (kbd "C-x c M-a") 'helm-multi-swoop-all)
+
+;; Local Variables:
+;; mode: emacs-lisp
+;; coding: utf-8-emacs-unix
+;; indent-tabs-mode: nil
+;; byte-compile-warnings: (not free-vars unresolved mapcar constants)
+;; End:
+
+;;; init-helm-swoop.el ends here
