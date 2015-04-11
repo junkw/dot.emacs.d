1a22f3c1798e3cf5d2fd47fccafc8205f6b629d1
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2015-04-11 18:24:31 +0900

    Install magit-svn because it was separated from magit

--- modules/opt-init-packages.el
+++ modules/opt-init-packages.el
@@ -58,7 +58,7 @@
                 elisp-slime-nav emmet-mode expand-region foreign-regexp flycheck geben gist
                 git-gutter-fringe goto-chg grep-a-lot helm-ag helm-c-yasnippet helm-ls-git
                 helm-migemo helm-swoop highlight-defined highlight-symbol info+ js2-mode
-                linum-relative lispxmp magit markdown-mode migemo monokai-emacs
+                linum-relative lispxmp magit magit-svn markdown-mode migemo monokai-emacs
                 multiple-cursors neotree org-mode php-completion php-mode projectile psvn
                 rainbow-mode recentf-ext scratch-ext smart-newline tern viewer web-mode
                 wgrep yaml-mode)
