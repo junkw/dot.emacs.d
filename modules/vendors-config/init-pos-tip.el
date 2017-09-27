adf1a89c8035d5a6c8072cb6876ffde976fbc20d
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2017-09-27 23:37:51 +0900

    Add pos-tip's color config

---
+++ modules/vendors-config/init-pos-tip.el
@@ -0,0 +1,44 @@
+;;; init-pos-tip.el --- el-get init file for package pos-tip
+
+;; Copyright (C) 2017  Jumpei KAWAMI
+
+;; Author: Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
+;; Created: Sep. 27, 2017
+;; Keywords: .emacs, popup
+
+;;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Change Log:
+;;
+
+;;; Code:
+
+(setq pos-tip-foreground-color "#abb2bf")
+(setq pos-tip-background-color "#282c34")
+
+;; Local Variables:
+;; mode: emacs-lisp
+;; coding: utf-8-emacs-unix
+;; indent-tabs-mode: nil
+;; byte-compile-warnings: (not free-vars unresolved mapcar constants)
+;; End:
+
+;;; init-pos-tip.el ends here
