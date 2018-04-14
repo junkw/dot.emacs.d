e854f27c96248b634acdbb80c24790bb003f4d4f
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2018-04-14 13:53:13 +0900

    Change `ac-php-tags-path' from XDG_CACHE_HOME to XDG_DATA_HOME

--- modules/vendors-config/init-ac-php.el
+++ modules/vendors-config/init-ac-php.el
@@ -37,7 +37,7 @@
 (with-eval-after-load 'company
   (require 'company-php))
 
-(setq ac-php-tags-path (substitute-in-file-name "$XDG_CACHE_HOME/ac-php"))
+(setq ac-php-tags-path (substitute-in-file-name "$XDG_DATA_HOME/ac-php"))
 
 ;;;; Hooks
 (defun jkw:ac-php-hooks ()
