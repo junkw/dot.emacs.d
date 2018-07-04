6bca77542bb085911c487484bb50fc67f81c01fb
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2018-07-04 12:58:18 +0900

    config: add rg's config

---
+++ modules/vendors-config/init-rg.el
@@ -0,0 +1,49 @@
+;;; init-rg.el --- el-get init file for package rg
+
+;; (C) 2018  Jumpei KAWAMI
+
+;; Author: Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
+;; Created: Jul. 4, 2018
+;; Keywords: .emacs, rg
+
+;;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Change Log:
+;;
+
+;;; Code:
+
+(setq rg-ignore-case 'smart)
+
+;;;; Hooks
+(add-hook 'rg-mode-hook #'wgrep-ag-setup)
+
+;;;; Keymap
+(rg-enable-default-bindings (kbd "M-s"))
+
+;; Local Variables:
+;; mode: emacs-lisp
+;; coding: utf-8-emacs-unix
+;; indent-tabs-mode: nil
+;; byte-compile-warnings: (not free-vars unresolved mapcar constants)
+;; End:
+
+;;; init-rg.el ends here
